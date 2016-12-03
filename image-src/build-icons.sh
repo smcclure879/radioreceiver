cff56b257b8562230f98abcfcf862f047592fe70
ayvex light industries llc <ayvexllc@gmail.com>
2016-12-03 02:49:18 -0800

    success...can edit cloned existing chrome apps 	modified:   ../extension/icon-128.png 	modified:   ../extension/icon-16.png 	modified:   ../extension/icon-48.png 	modified:   ../extension/interface.html 	modified:   ../extension/manifest.json 	new file:   build-icons.sh 	modified:   icon.svg

---
+++ image-src/build-icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+convert icon.svg -size 16x16 ../extension/icon-16.png 
+convert icon.svg -size 48x48 ../extension/icon-48.png 
+convert icon.svg -size 128x128 ../extension/icon-128.png 
+
